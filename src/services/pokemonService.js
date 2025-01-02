acdbdcbe4ef4cc05892da6989ec509257d4e5ee8
Ram Akasaka <142519662+ihell@users.noreply.github.com>
2025-01-02 20:57:59 +0700

    fix: update Pokémon API limit to fetch all available Pokémon

--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -1,5 +1,5 @@
 export const fetchPokemons = async () => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
   const data = await response.json();
   return data.results;
 };
